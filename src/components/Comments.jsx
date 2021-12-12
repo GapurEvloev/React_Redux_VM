1fe2508977193c9af03ee4618b50e6c761e48b5a
Gapur Yovloy <kaloycant@gmail.com>
2021-12-12 12:51:02 +0300

    1:32:30

---
+++ src/components/Comments.jsx
@@ -0,0 +1,41 @@
+import {useState} from "react";
+import { useDispatch, useSelector } from "react-redux";
+import uniqid from "uniqid";
+import SingleComment from "./SingleComment";
+import { commentCreate } from "../redux/actions";
+
+function Comments (props) {
+    const [textComment, setTextComment] = useState('');
+    const comments = useSelector(state => {
+        const { commentsReducer } = state;
+        return commentsReducer.comments;
+    });
+
+    const dispatch = useDispatch();
+
+    const handleInput = (e) => {
+        setTextComment(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submit text comment >>> ", textComment);
+        const id = uniqid();
+        dispatch(commentCreate(textComment, id))
+    }
+
+    return (
+        <div className="card-comments">
+            <form onSubmit={handleSubmit} action="" className="comments-item-create">
+                <input type="text" value={textComment} onChange={handleInput} />
+                <input type="submit" hidden />
+            </form>
+            {!!comments.length && comments.map(res => {
+                return <SingleComment key={res.id} data={res} />;
+            })}
+            
+        </div>
+    )
+}
+
+export default Comments;
